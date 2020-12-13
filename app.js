5b4e369219c6c0eb242d5b184424a9c9bbba8f2b
Michael Berryman <berryman.michael@yahoo.com>
2020-12-13 15:58:10 -0800

    Make reset button functionable

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
+//Constants
 const container = document.querySelector('#container');
 const reset = document.querySelector('.reset');
-
-for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
-}
-
 const grid = document.querySelectorAll('.square');
 
-grid.forEach((square) => {
-   square.addEventListener('mouseenter', () => {
-        square.classList.add('blue');
-   });
+//Functions
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function newGrid() {
+     let gridNumber = Number(prompt('How many squares per side would you like?'));
+     startingGrid(gridNumber);
+}
+
+function startingGrid(gridNumber) {
+    let gridArea = gridNumber * gridNumber;
+
+    for (let i = 0; i < gridArea; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+
+        container.insertAdjacentElement('beforeend', square);
+
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('blue');
+        });
+    }
+}
+
+//Listeners
+reset.addEventListener('click', () => {
+    removeAllChildNodes(container);
+    newGrid();
 });
 
-reset.addEventListener('click', () => {
-   console.log('clicked button');
-});
+startingGrid(16);
